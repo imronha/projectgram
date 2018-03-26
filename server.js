c66651514ce7d8e040d318d23b09ebceeb81a1ec
Imron Hajiahmad <imronha@gmail.com>
2018-03-26 01:21:33 -0700

    server.js created, first build

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express'); //Express server
+const bodyParser = require('body-parser'); //Allows data extraction from request headers, etc
+const path = require('path'); //Directory path tool
+const http = require('http');
+const app = express();
+
+//Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//Serve static files
+app.use(express.static(path.join(__dirname,'dist')));
+
+//Set up our api routes
+//app.use('/api', api);
+
+//Return other routes to Angular index file
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+//Create HTTP server
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Running on localhost:' + port))
